241edeb3794b68390ab7fb78c0f21ce5887a29d7
leejs1030 <leejs1030@korea.ac.kr>
2023-09-22 08:41:08 +0900

    feat: 레거시 코드 제거

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
 import { ResolveJoiSchema } from './types/resolve-joi-schema';
-import { TypedConfigModule } from './typed-config.module';
-import { BaseTypedConfigService } from './base-typed-config.service';
 import { createTypedConfig } from './create-typed-config';
 
-export {
-  ResolveJoiSchema,
-  TypedConfigModule,
-  BaseTypedConfigService,
-  createTypedConfig,
-};
+export { ResolveJoiSchema, createTypedConfig };
