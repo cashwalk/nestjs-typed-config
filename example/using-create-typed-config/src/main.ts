9ad652c9f4bf8982deab21a05e5011e6949d1633
leejs1030 <leejs1030@korea.ac.kr>
2023-09-22 09:05:31 +0900

    feat: (example code) use TypedConfigService to get PORT

--- example/using-create-typed-config/src/main.ts
+++ example/using-create-typed-config/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TypedConfigService } from './typed-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(TypedConfigService); // use TypedConfigService from your typed-config
+  const PORT = configService.get('PORT'); // infer type automatically
+  await app.listen(PORT);
 }
 bootstrap();
