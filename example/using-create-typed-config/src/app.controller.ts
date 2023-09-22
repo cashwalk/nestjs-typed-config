6c45a7d996ca01773f5059cd7046945d991e6534
leejs1030 <leejs1030@korea.ac.kr>
2023-09-22 09:05:12 +0900

    feat: (example code) add and remove modules

--- example/using-create-typed-config/src/app.controller.ts
+++ example/using-create-typed-config/src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { TypedConfigService } from './typed-config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly configService: TypedConfigService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.configService.get('HELLO_MESSAGE');
   }
 }
--- example/using-create-typed-config/src/app.module.ts
+++ example/using-create-typed-config/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServerInfoModule } from './server-info/server-info.module';
 
 @Module({
-  imports: [],
+  imports: [ServerInfoModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
